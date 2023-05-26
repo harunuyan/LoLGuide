51d8e9852e3418af7fde02e3d913ec470bb4ec5f
harun <harunuyan6@gmail.com>
2023-05-26 23:51:22 +0300

    Added Bottom Navigation Menu & Nav Host Fragment

--- app/src/main/java/com/volie/lolguidestats/MainActivity.kt
+++ app/src/main/java/com/volie/lolguidestats/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.volie.lolguidestats
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.volie.lolguidestats.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private var _mBinding: ActivityMainBinding? = null
+    private val mBinding get() = _mBinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        mBinding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mBinding = null
     }
 }
