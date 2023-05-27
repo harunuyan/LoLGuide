904dce89962a41a2f78cd6e42679814618c11373
harun <harunuyan6@gmail.com>
2023-05-27 18:03:50 +0300

    Designed Champions Details Layout

---
+++ app/src/main/java/com/volie/lolguidestats/ui/fragment/champ_detail/ChampionsDetailsFragment.kt
@@ -0,0 +1,27 @@
+package com.volie.lolguidestats.ui.fragment.champ_detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.volie.lolguidestats.databinding.FragmentChampionsDetailsBinding
+
+class ChampionsDetailsFragment : Fragment() {
+    private var _mBinding: FragmentChampionsDetailsBinding? = null
+    private val mBinding get() = _mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _mBinding = FragmentChampionsDetailsBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mBinding = null
+    }
+}
