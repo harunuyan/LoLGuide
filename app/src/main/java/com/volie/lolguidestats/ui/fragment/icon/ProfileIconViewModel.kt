b9ca14be2f3639ab3c9d74db50c00f82014c564c
harun <harunuyan6@gmail.com>
2023-06-03 20:23:59 +0300

    Added profile icon view model

---
+++ app/src/main/java/com/volie/lolguidestats/ui/fragment/icon/ProfileIconViewModel.kt
@@ -0,0 +1,30 @@
+package com.volie.lolguidestats.ui.fragment.icon
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.volie.lolguidestats.data.model.profile_icon.IconData
+import com.volie.lolguidestats.data.remote.Repository
+import com.volie.lolguidestats.helper.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileIconViewModel
+@Inject constructor(
+    private val repository: Repository
+) : ViewModel() {
+
+    private val _icons = MutableLiveData<Resource<IconData>>()
+    val icons get() = _icons
+
+    fun getProfileIcons() {
+        _icons.postValue(Resource.loading(null))
+        viewModelScope.launch(Dispatchers.IO) {
+            val result = repository.getProfileIcons()
+            _icons.postValue(result)
+        }
+    }
+}
