d577638af52188c07d1cb89923a4e12e9ca74c87
harun <harunuyan6@gmail.com>
2023-06-03 20:01:56 +0300

    Added profile icon fragment

---
+++ app/src/main/java/com/volie/lolguidestats/ui/fragment/icon/ProfileIconFragment.kt
@@ -0,0 +1,8 @@
+package com.volie.lolguidestats.ui.fragment.icon
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProfileIconFragment : Fragment() {
+}
