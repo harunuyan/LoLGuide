c850117a2892cdd6cd166e1e8120f5990034195f
harun <harunuyan6@gmail.com>
2023-06-03 18:13:18 +0300

    Added Summoner Spell Fragment Recycler View Adapter

---
+++ app/src/main/java/com/volie/lolguidestats/ui/fragment/summoner_spell/SummonerSpellRVAdapter.kt
@@ -0,0 +1,62 @@
+package com.volie.lolguidestats.ui.fragment.summoner_spell
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.volie.lolguidestats.data.model.summoner_spell.SummonerSpell
+import com.volie.lolguidestats.databinding.ItemSummonerSpellBinding
+import com.volie.lolguidestats.helper.Constant.SPELL_IMAGE
+import com.volie.lolguidestats.ui.adapter.BaseAdapter
+
+class SummonerSpellRVAdapter : BaseAdapter<SummonerSpell>(SummonerSpellDiffCallback()) {
+
+    inner class SummonerSpellViewHolder(private val binding: ItemSummonerSpellBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            val spell = currentList[position]
+            Glide.with(binding.root)
+                .load("${SPELL_IMAGE}${spell.id}.png")
+                .into(binding.ivItemImage)
+
+            binding.tvItemName.text = spell.name
+            binding.tvItemPlain.text = spell.description
+            binding.tvLevel.text = "Level ${spell.summonerLevel.toString()}"
+            binding.tvItemMode.text = spell.modes.toString().trim('[', ']')
+        }
+    }
+
+    override fun createViewHolder(
+        parent: ViewGroup,
+        inflater: LayoutInflater,
+        viewType: Int
+    ): RecyclerView.ViewHolder {
+        val binding = ItemSummonerSpellBinding.inflate(inflater, parent, false)
+        return SummonerSpellViewHolder(binding)
+    }
+
+    override fun bindViewHolder(
+        holder: RecyclerView.ViewHolder,
+        item: SummonerSpell,
+        position: Int
+    ) {
+        if (holder is SummonerSpellViewHolder) {
+            holder.bind(position)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return currentList.size
+    }
+}
+
+class SummonerSpellDiffCallback : DiffUtil.ItemCallback<SummonerSpell>() {
+    override fun areItemsTheSame(oldItem: SummonerSpell, newItem: SummonerSpell): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: SummonerSpell, newItem: SummonerSpell): Boolean {
+        return oldItem == newItem
+    }
+}
