d6a50d9a6e44400228941a3601fe69fe3e0ee8e9
harun <harunuyan6@gmail.com>
2023-06-03 20:24:55 +0300

    Displayed Profile Icons to ui

--- app/src/main/java/com/volie/lolguidestats/helper/Constant.kt
+++ app/src/main/java/com/volie/lolguidestats/helper/Constant.kt
@@ -7,4 +7,6 @@ object Constant {
     const val CHAMPION_URL = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"
     const val ITEM_IMAGE = "https://ddragon.leagueoflegends.com/cdn/13.10.1/img/item/"
     const val SPELL_IMAGE = "https://ddragon.leagueoflegends.com/cdn/13.10.1/img/spell/"
+    const val PROFILE_ICON_IMAGE =
+        "https://ddragon.leagueoflegends.com/cdn/13.10.1/img/profileicon/"
 }
--- app/src/main/java/com/volie/lolguidestats/ui/fragment/icon/ProfileIconFragment.kt
+++ app/src/main/java/com/volie/lolguidestats/ui/fragment/icon/ProfileIconFragment.kt
@@ -1,8 +1,58 @@
 package com.volie.lolguidestats.ui.fragment.icon
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.volie.lolguidestats.databinding.FragmentProfileIconBinding
+import com.volie.lolguidestats.helper.Status
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ProfileIconFragment : Fragment() {
+    private var _mBinding: FragmentProfileIconBinding? = null
+    private val mBinding get() = _mBinding!!
+    private val mAdapter = ProfileIconRVAdapter()
+    private val mViewModel: ProfileIconViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _mBinding = FragmentProfileIconBinding.inflate(inflater, container, false)
+        mBinding.rvProfileIcon.adapter = mAdapter
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeLiveData()
+        mViewModel.getProfileIcons()
+    }
+
+    private fun observeLiveData() {
+        mViewModel.icons.observe(viewLifecycleOwner) { resource ->
+            when (resource.status) {
+                Status.SUCCESS -> {
+                    resource.data?.let {
+                        val iconMap = it.data
+                        val iconList = iconMap.values.toList()
+                        mAdapter.submitList(iconList)
+                    }
+                }
+
+                Status.ERROR -> {}
+                Status.LOADING -> {}
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mBinding = null
+    }
 }
