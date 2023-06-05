ccbe6a0b7338f85e4269f5918316bb4e29818393
harun <harunuyan6@gmail.com>
2023-06-05 19:07:53 +0300

    Added model classes for Map

---
+++ app/src/main/java/com/volie/lolguidestats/data/model/map/Map.kt
@@ -0,0 +1,8 @@
+package com.volie.lolguidestats.data.model.map
+
+import com.volie.lolguidestats.data.model.champion.Image
+
+data class Map(
+    val mapName: String,
+    val image: Image,
+)
---
+++ app/src/main/java/com/volie/lolguidestats/data/model/map/MapData.kt
@@ -0,0 +1,7 @@
+package com.volie.lolguidestats.data.model.map
+
+import kotlin.collections.Map
+
+data class MapData(
+    val data: Map<String, com.volie.lolguidestats.data.model.map.Map>
+)
