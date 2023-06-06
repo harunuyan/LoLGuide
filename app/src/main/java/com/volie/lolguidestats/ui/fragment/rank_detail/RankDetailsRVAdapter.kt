423e6762af495ab03037f302a930a0bbf4413ffa
harun <harunuyan6@gmail.com>
2023-06-06 21:23:12 +0300

    Added rank details recycler view adapter

---
+++ app/src/main/java/com/volie/lolguidestats/ui/fragment/rank_detail/RankDetailsRVAdapter.kt
@@ -0,0 +1,54 @@
+package com.volie.lolguidestats.ui.fragment.rank_detail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.volie.lolguidestats.data.model.rank.Rank
+import com.volie.lolguidestats.databinding.ItemRankBinding
+import com.volie.lolguidestats.ui.adapter.BaseAdapter
+
+class RankDetailsRVAdapter : BaseAdapter<Rank>(RankDiffUtilCallback()) {
+
+    inner class RankViewHolder(private val binding: ItemRankBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            val rank = currentList[position]
+            Glide.with(binding.root)
+                .load(rank.url)
+                .into(binding.ivRank)
+
+            binding.tvRank.text = rank.rankName
+        }
+    }
+
+    override fun createViewHolder(
+        parent: ViewGroup,
+        inflater: LayoutInflater,
+        viewType: Int
+    ): RecyclerView.ViewHolder {
+        val binding = ItemRankBinding.inflate(inflater, parent, false)
+        return RankViewHolder(binding)
+    }
+
+    override fun bindViewHolder(holder: RecyclerView.ViewHolder, item: Rank, position: Int) {
+        if (holder is RankViewHolder) {
+            holder.bind(position)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return currentList.size
+    }
+}
+
+class RankDiffUtilCallback : DiffUtil.ItemCallback<Rank>() {
+    override fun areItemsTheSame(oldItem: Rank, newItem: Rank): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: Rank, newItem: Rank): Boolean {
+        return oldItem == newItem
+    }
+}
